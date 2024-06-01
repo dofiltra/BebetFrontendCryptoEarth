255fca815bbaa03ad356d0691937228912cf9df0
Your Name <you@example.com>
2024-06-01 12:48:49 +0000

    TanStackRouterDevtools - only for dev

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,12 +1,10 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-import App from "@/Components/App/App";
+import { createLazyFileRoute } from '@tanstack/react-router'
+import App from '@/Components/App/App'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
 function Index() {
-  return (
-    <App/>
-  )
+  return <App />
 }
