bf86280d1b68f2de3f5b5733837c5cdcb517e7ec
Your Name <you@example.com>
2024-06-01 12:32:18 +0000

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin(), TanStackRouterVite()],
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 });
