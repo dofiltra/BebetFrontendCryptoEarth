de767565ba3a69b61fc5089f323e4663ac875b91
Dmitrii Osipovich <menemty@gmail.com>
2024-06-02 16:59:21 +0300

    Что-то пошло не так

--- src/Components/App/Links/Links.tsx
+++ src/Components/App/Links/Links.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/shared/ui/button'
 import Input from '@/shared/ui/input'
 import { createFormData, postFormData } from '../../../services/api'
 import { type UserDto } from '@/entities/user'
+import { toast } from 'react-toastify'
 
 interface IStatisticProps {
   user: UserDto
@@ -19,24 +20,26 @@ const Links: React.FC<IStatisticProps> = ({ user, getRefUrls, refLinks }) => {
 
   const createUrl = async () => {
     const url = `${link}`
-    const d = createFormData({ name: name, description: description, adress: url })
+    const d = createFormData({ name, description, adress: url })
     const res = await postFormData('/ref_refs/Create', d)
 
-    if (res) {
-      setShowPopup(false)
-      getRefUrls()
+    if (!res) {
+      return toast.warning('Что-то пошло не так')
     }
+
+    setShowPopup(false)
+    getRefUrls()
   }
 
   const popup = () => {
     return (
       <div className={'links-popup'} style={{ width: '100%', margin: 0 }}>
         <div className={'login__closeBtn'} onClick={() => setShowPopup(false)}>
-          <svg width='48' height='48' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <rect width='48' height='48' rx='16' fill='white' />
+          <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="48" height="48" rx="16" fill="white" />
             <path
-              d='M28.331 18.6083C28.6239 18.3154 29.0988 18.3154 29.3917 18.6083C29.6846 18.9012 29.6846 19.3761 29.3917 19.669L25.0606 24L29.3916 28.331C29.6845 28.6239 29.6845 29.0988 29.3916 29.3917C29.0987 29.6846 28.6238 29.6846 28.331 29.3917L23.9999 25.0607L19.6689 29.3917C19.376 29.6846 18.9012 29.6846 18.6083 29.3917C18.3154 29.0988 18.3154 28.6239 18.6083 28.331L22.9393 24L18.6082 19.669C18.3153 19.3761 18.3153 18.9012 18.6082 18.6083C18.9011 18.3154 19.376 18.3154 19.6689 18.6083L23.9999 22.9394L28.331 18.6083Z'
-              fill='black'
+              d="M28.331 18.6083C28.6239 18.3154 29.0988 18.3154 29.3917 18.6083C29.6846 18.9012 29.6846 19.3761 29.3917 19.669L25.0606 24L29.3916 28.331C29.6845 28.6239 29.6845 29.0988 29.3916 29.3917C29.0987 29.6846 28.6238 29.6846 28.331 29.3917L23.9999 25.0607L19.6689 29.3917C19.376 29.6846 18.9012 29.6846 18.6083 29.3917C18.3154 29.0988 18.3154 28.6239 18.6083 28.331L22.9393 24L18.6082 19.669C18.3153 19.3761 18.3153 18.9012 18.6082 18.6083C18.9011 18.3154 19.376 18.3154 19.6689 18.6083L23.9999 22.9394L28.331 18.6083Z"
+              fill="black"
             />
           </svg>
         </div>
@@ -62,7 +65,6 @@ const Links: React.FC<IStatisticProps> = ({ user, getRefUrls, refLinks }) => {
       {showPopup && popup()}
       <div className={'links__title'}>
         <p>Мои ссылки</p>
-
         <Button title={'Создать ссылку'} handleClick={() => setShowPopup(true)} disabled={false} />
       </div>
       <div className={'links__container'}>
