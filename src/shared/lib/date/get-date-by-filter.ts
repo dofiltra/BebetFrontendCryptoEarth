82d567744788ec2eca7c48cee86c69e569a7cfda
Dmitrii Osipovich <menemty@gmail.com>
2024-06-02 14:12:09 +0300

    fix getDateByFilter

--- src/shared/lib/date/get-date-by-filter.ts
+++ src/shared/lib/date/get-date-by-filter.ts
@@ -1,28 +1,27 @@
 import { Filters } from '@/Components/App/Statistic/Statistic.types'
 
 export function getDateByFilter(dateValue?: Filters): Date {
-  const currentDate = new Date()
+  const now = new Date()
 
   if (!dateValue || dateValue === 'all') {
-    const startOfYear = new Date(currentDate)
-    startOfYear.setDate(currentDate.getDate() - 365)
+    const startOfYear = new Date(now)
+    startOfYear.setDate(now.getDate() - 365)
     return startOfYear
   }
 
   switch (dateValue) {
     case 'month': {
-      return new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)
+      return new Date(now.getFullYear(), now.getMonth(), 1)
     }
-    case 'week':
-      const startOfWeek = new Date(currentDate)
-      startOfWeek.setDate(currentDate.getDate() - currentDate.getDay())
-      return startOfWeek
-    case 'yesterday':
-      const yesterday = new Date(currentDate)
-      yesterday.setDate(currentDate.getDate() - 1)
-      return yesterday
-    case 'today':
-      return currentDate
+    case 'week': {
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay())
+    }
+    case 'yesterday': {
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
+    }
+    case 'today': {
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    }
     default:
       throw new Error('Invalid dateValue. Accepted values are "month", "week", "yesterday", or "today".')
   }
